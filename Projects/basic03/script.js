c8293f28ade19bd635e47cb6d0ad85b5ca49a44a
Ritik Shrivastav <118468793+RitikSrivastav24@users.noreply.github.com>
2025-12-04 07:20:32 +0000

    more ways to display clock

--- Projects/basic03/script.js
+++ Projects/basic03/script.js
@@ -1,7 +1,25 @@
 const clock = document.querySelector('#clock');
 
-setInterval(function () {
-  let date = new Date();
-  //console.log(date.toLocaleTimeString());
-  clock.innerHTML = date.toLocaleTimeString();
-}, 1000);
+// setInterval(function () {
+//   let date = new Date();
+//   //console.log(date.toLocaleTimeString());
+//   clock.innerHTML = date.toLocaleTimeString();
+// }, 1000);
+
+
+//second way
+// function updateClock() {
+//   let date = new Date();
+//   clock.innerHTML = date.toLocaleTimeString();
+// }
+// setInterval(updateClock, 1000);
+// updateClock();
+
+//third way
+
+function animateClock() {
+  clock.textContent = new Date().toLocaleTimeString();
+  requestAnimationFrame(animateClock);
+}
+
+animateClock();
